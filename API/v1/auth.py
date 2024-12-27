ddc0d3f18aebaf36b2a0dfc35f856077ea4b0cae
serdy <Serdyukov_007@mail.ru>
2024-12-27 19:46:37 +0300

    add check metod

--- API/v1/auth.py
+++ API/v1/auth.py
@@ -17,7 +17,7 @@ async def nuw_user(wallet_address, tgInitData):
     try:
         id = str(WebAppAuth().get_user_id(tgInitData))
     except AuthError as e:
-        return {'state': 'invalid', 'op': '-100', 'detail': e.detail}
+        return {'state': 'invalid', 'op': '-100', 'reason': e.detail}
     id2user[id]: dict = json.loads(WebAppAuth().validate(tgInitData)['user'])
 
     wallet_old = id2wallet.get(id)
@@ -32,6 +32,28 @@ async def nuw_user(wallet_address, tgInitData):
     await storage.set_item('id2user', json.dumps(id2user))
 
     return {'state': 'success', 'op': '0'}
+
+
+@auth_router.get("/check")
+async def check(wallet_address: str | None = None, wallets_addresses: list[str] | None = None):
+    if not wallet_address and not wallets_addresses:
+        return {'state': 'invalid', 'op': '-100', 'reason': 'Not given wallet address'}
+
+    wallet2id: dict = json.loads(await storage.get_item('wallet2id'))
+
+    if wallets_addresses:
+        wallets_valid = []
+        for wallet in wallets_addresses:
+            if wallet2id.get(wallet):
+                wallets_valid.append((wallet, 0))
+            else:
+                wallets_valid.append((wallet, 1))
+        return {'state': 'success', 'op': '0', 'data': wallets_valid}
+
+    if wallet2id.get(wallet_address):
+        return {'state': 'success', 'op': '0', 'data': [(wallet_address, 0)]}
+    else:
+        return {'state': 'success', 'op': '0', 'data': [(wallet_address, 1)]}
 
 
 @auth_router.get("/validate")
@@ -68,10 +90,9 @@ async def remove_from_banlist(user: str):
 
 
 async def main():
-    await storage.set_item('id2wallet', '{}')
-    await storage.set_item('wallet2id', '{}')
-    await storage.set_item('id2user', '{}')
-    await storage.set_item('banlist', '{}')
+    print(await check(wallet_address='0:d5bbeb92a3de5926bfb523aa3b1876f5507baca17d533ecfc9a9680a94e4039e'))
+    print(await check(wallets_addresses=['0:d5bbeb92a3de5926bfb523aa3b1876f5507baca17d533ecfc9a9680a94e4039e',
+                                         'UQB7q5qYhCOAIQbhWlIoq78ZXpGZWRn-gZc30-CSckZSMc_s']))
 
 
 if __name__ == '__main__':
