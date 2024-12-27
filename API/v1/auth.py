702f5b42903c69f2769103c43f20b4db7cf84b5d
serdy <Serdyukov_007@mail.ru>
2024-12-27 20:42:20 +0300

    update check metod

--- API/v1/auth.py
+++ API/v1/auth.py
@@ -35,25 +35,26 @@ async def nuw_user(wallet_address, tgInitData):
 
 
 @auth_router.get("/check")
-async def check(wallet_address: str | None = None, wallets_addresses: list[str] | None = None):
-    if not wallet_address and not wallets_addresses:
+async def check(addresses: str | None = None):
+    # INPUT
+    # wallets_addresses     must be:    addr1|addr2|addr3
+    # addresses in raw
+
+    # OUTPUT
+    # return only valid
+    # {'state': 'success', 'op': '0', 'data': [addr1,addr3]}
+
+    if not addresses:
         return {'state': 'invalid', 'op': '-100', 'reason': 'Not given wallet address'}
 
     wallet2id: dict = json.loads(await storage.get_item('wallet2id'))
 
-    if wallets_addresses:
+    if addresses:
         wallets_valid = []
-        for wallet in wallets_addresses:
+        for wallet in addresses.split('|'):
             if wallet2id.get(wallet):
-                wallets_valid.append((wallet, 0))
-            else:
-                wallets_valid.append((wallet, 1))
+                wallets_valid.append(wallet)
         return {'state': 'success', 'op': '0', 'data': wallets_valid}
-
-    if wallet2id.get(wallet_address):
-        return {'state': 'success', 'op': '0', 'data': [(wallet_address, 0)]}
-    else:
-        return {'state': 'success', 'op': '0', 'data': [(wallet_address, 1)]}
 
 
 @auth_router.get("/validate")
