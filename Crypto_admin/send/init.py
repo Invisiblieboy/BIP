687fc4f58eb81fe8d152db077d5c7b6c66462156
serdy <Serdyukov_007@mail.ru>
2024-12-29 18:40:07 +0300

    add Storage

--- Crypto_admin/send/init.py
+++ Crypto_admin/send/init.py
@@ -1,8 +1,31 @@
+import redis.asyncio as redis
 
-from data.storage import storage
+client = redis.Redis(host='localhost', port=6379)
+
+
+class Storage:
+    base_key = 'BIP_'
+
+    async def set_item(self, key: str, value: str):
+        await client.set(name=self.base_key + key, value=value)
+
+    async def get_item(self, key: str, default_value: str = None):
+        value = await client.get(name=self.base_key + key)
+        return value.decode() if value else default_value
+
+    async def remove_item(self, key: str):
+        await client.delete(self.base_key + key)
+
+    async def clear(self):
+        for key in await client.keys(f'{self.base_key}*'):
+            await client.delete(key)
+
+
+storage = Storage()
+
+
 async def main():
-    await storage.set_item('processed_transactions', 'first')
-    await storage.set_item('last_transaction_timestamp', 1734889015)
+    await storage.set_item('last_transaction_timestamp', '1735427301')
 
 
 if __name__ == '__main__':
