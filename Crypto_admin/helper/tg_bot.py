eb5c0ed6a7f5e84c724b69e48d68e2727c4c9a91
serdy <Serdyukov_007@mail.ru>
2024-12-29 18:46:46 +0300

    bug fix

--- Crypto_admin/helper/tg_bot.py
+++ Crypto_admin/helper/tg_bot.py
@@ -115,5 +115,10 @@ async def main():
     await dp.start_polling(bot)
 
 
+def start():
+    import asyncio
+    asyncio.run(main())
+
+
 if __name__ == '__main__':
     asyncio.run(main())
--- Crypto_admin/main.py
+++ Crypto_admin/main.py
@@ -2,9 +2,11 @@ import threading
 import time
 
 from send import appTransactions
+from helper import tg_bot
 
 if __name__ == '__main__':
     threading.Thread(target=appTransactions._start, args=[1, ], daemon=True).start()
+    threading.Thread(target=tg_bot.main, daemon=True).start()
 
     while 1:
         time.sleep(100)
