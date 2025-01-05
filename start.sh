5cb071eac0d19af6b7ae5d489c11c475eb9def02
serdy <Serdyukov_007@mail.ru>
2025-01-05 03:17:11 +0300

    normal start

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@ nginx -s reload
 
 killall python uvicorn
 
-cd /usr/dev/BIP
+cd /usr/dev/BIP/Crypto_admin
 python -m main &
+cd /usr/dev/BIP/API
+python -m main &
+cd /usr/dev/BIP/Tg_bot
+python -m tg_bot &
 
 cd /usr/dev/BIP/API
 uvicorn main:app --port 8000 --no-access-log &
