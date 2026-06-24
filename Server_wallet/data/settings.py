c0d44771e7b3aaa53450947655f1fcdb5ba0c92b
serdy <Serdyukov_007@mail.ru>
2026-06-25 00:40:22 +0300

    bug fix and better cod

--- Server_wallet/data/settings.py
+++ Server_wallet/data/settings.py
@@ -1,5 +1,12 @@
-proxies = [None]
+import os
 
+from dotenv import load_dotenv
+
+proxies = [None]
 
 BIP_address = '0:786e74bba071d57c75004bb41bf978ea73e00b03f3f3fbf2b73ea0426f97d6e9'
 BIP_decimals = 1e9
+
+load_dotenv()
+API_ID = os.getenv('api_id')
+API_HASH = os.getenv('api_hash')
--- Server_wallet/main.py
+++ Server_wallet/main.py
@@ -1,8 +1,7 @@
 import asyncio
 import time
-from pprint import pprint
 
-from add_bonuses import add_member_daily_bonus, add_member_percents_by_cw_balance
+from add_bonuses import add_member_daily_bonus, add_member_percents_by_cw_balance, add_member_percents_by_sw_balance
 from storage import storage as redis_storage
 from utils import get_seconds_until_tomorrow_utc
 
@@ -16,9 +15,12 @@ async def auto_add_bonuses():
         try:
             await add_member_percents_by_cw_balance()
         except Exception as e:
-            print(time.time(), 'add_member_daily_bonus', e)
+            print(time.time(), 'add_member_percents_by_cw_balance', e)
+        try:
+            await add_member_percents_by_sw_balance()
+        except Exception as e:
+            print(time.time(), 'add_member_percents_by_sw_balance', e)
         await redis_storage.save('user_data', 'user_data')
-        pprint(await redis_storage.get_item('user_data'))
         await asyncio.sleep(get_seconds_until_tomorrow_utc())
 
 
@@ -27,7 +29,6 @@ async def main():
     await redis_storage.save('user_data', 'user_data')
     # await asyncio.sleep(get_seconds_until_tomorrow_utc())
     task1 = asyncio.create_task(auto_add_bonuses())
-    pprint('a')
     await task1
 
 
