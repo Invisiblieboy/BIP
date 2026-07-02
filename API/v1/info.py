ac0c7f7e258a5d3652492e17817d326073d359a1
serdy <Serdyukov_007@mail.ru>
2026-07-02 19:06:45 +0300

    better syntax

--- API/v1/info.py
+++ API/v1/info.py
@@ -7,16 +7,16 @@ from utils.config import payments
 info_router = APIRouter(prefix='/info', tags=['info'])
 
 
-@info_router.get("/time")
+@info_router.get("/time", summary='Now time')
 async def bip_price():
     return {"time": str(time.time())}
 
 
-@info_router.get("/card")
+@info_router.get("/card", summary='Card payment details')
 async def bip_price():
     return {"card": payments['card']}
 
 
-@info_router.get("/payments")
+@info_router.get("/payments", summary='All payment')
 async def bip_price():
     return payments
